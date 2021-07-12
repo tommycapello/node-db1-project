c8335119f89518c7a96f67be1fb83c11759713fe
tommycapello <thomcapello@gmail.com>
2021-07-12 19:29:46 -0400

    setting up routes and middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,29 @@
+const Account = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+next()
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+next()
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+    try{
+        const account = await Account.getById(req.params.id)
+        if(!account){
+            next({
+                status: 404,
+                message: 'Account not found'
+            })
+        }
+        else{
+            req.account = account
+            next()
+        }
+    }
+    catch(err){
+        next(err)
+    }
+
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,54 @@
 const router = require('express').Router()
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+const md = require('./accounts-middleware')
+
+const Account = require('./accounts-model')
+
+router.get('/', async (req, res, next) => {
+  try{
+    const accounts = await Account.getAll()
+    res.json(accounts)
+  }
+  catch(err){
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', md.checkAccountId, async (req, res, next) => {
+  res.json(req.account)
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  try{
+
+  }
+  catch(err){
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', md.checkAccountId, (req, res, next) => {
+  try{
+
+  }
+  catch(err){
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', md.checkAccountId, (req, res, next) => {
+  try{
+
+  }
+  catch(err){
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message
+  })
 })
 
 module.exports = router;
